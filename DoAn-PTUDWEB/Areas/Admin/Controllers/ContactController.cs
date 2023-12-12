05e5fa00a5da0af2f94a0e761daca359557df258
Bình Dương <121614918+binhduong03@users.noreply.github.com>
2023-12-12 15:23:27 +0700

    cập nhật Admin

---
+++ DoAn-PTUDWEB/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using DoAn_PTUDWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn_PTUDWEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactController : Controller
+    {
+        private readonly DataContext _context;
+        public ContactController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Contact()
+        {
+            var contactlist = _context.TbContacts.OrderBy(m => m.ContactId).ToList();
+            return View(contactlist);
+        }
+    }
+}
