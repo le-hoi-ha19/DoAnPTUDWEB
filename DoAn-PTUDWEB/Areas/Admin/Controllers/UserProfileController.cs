98777bf2494743630c33568d0b5322156f68b608
Bình Dương <121614918+binhduong03@users.noreply.github.com>
2024-01-10 12:07:55 +0700

    chỉnh css phần product, tradermark và thêm UserProfile
    1/10/2024

---
+++ DoAn-PTUDWEB/Areas/Admin/Controllers/UserProfileController.cs
@@ -0,0 +1,24 @@
+﻿using DoAn_PTUDWEB.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn_PTUDWEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserProfileController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            if (!Functions.IsLogin())
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+
+        public IActionResult Password()
+        {
+            return View();
+        }
+    }
+}
